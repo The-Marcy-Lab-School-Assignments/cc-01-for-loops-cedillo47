e2c74ff62bf529a40b8dff2dfdcbf73e693e1f45
leo <cedillo47@gmail.com>
2021-09-28 14:44:49 +0000

    attempt at 1,2,3

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 //1
 function countToFifty(){
-  
+  for (let i = 1; i < 51; i++){ 
+  var count = console.log(i); 
+  }
+  return count;
+
 }
 
 //2
-function countFromOne(){
-  
+function countFromOne(x){
+  for (let i = 1; i <= x; i++ )
+  var l = console.log(i); 
+  return l ;
 }
 
 //3
 function threeAndFiveInFifty(){
-  
+  for (let i = 1; i <= 50; i++){
+  if (i % 3 == 0 || i % 5 == 0 ){
+  console.log i;      
+ }
 }
 
 //4
