82d6f5bece33bd30e7904e0e1e06a417f2716734
leo <cedillo47@gmail.com>
2021-09-28 15:05:54 +0000

    attempts at 3 and 4

--- index.js
+++ index.js
@@ -18,13 +18,20 @@ function countFromOne(x){
 function threeAndFiveInFifty(){
   for (let i = 1; i <= 50; i++){
   if (i % 3 == 0 || i % 5 == 0 ){
-  console.log i;      
- }
+  console.log(i);      
+ };
+}
+    
 }
 
 //4
-function threeAndFive(){
-  
+function threeAndFive(x){
+  for (let i = 1; i <= x; i++){
+      if (i % 3 == 0 || i % 5 == 0 ){
+          console.log(i);
+      }
+  }
 }
 
 
+threeAndFiveInFifty();
